2aba410bc3afc4067a9af0aef82e307d853f1463
Jason Schwartz <jasonscottschwartz@gmail.com>
2018-08-29 16:47:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ function updatePlaylist(playlist, name, song){
   playlist[name] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, name){
+  delete playlist[name]
+  return playlist
+}
